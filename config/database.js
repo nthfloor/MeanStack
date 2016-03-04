340c9fb5ec3275c2996425679c26125ec482b39a
Nathan Floor <nathan.floor@gmail.com>
2016-03-04 22:18:33 +0200

    resolved the routing issue. Was the database connection. Also added validation to check if db connected ok.

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
 module.exports = {
-    url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'
+    // Connection URL. This is where your mongodb server is running.
+    modulud_url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu',
+    url : 'mongodb://localhost:27017/todo_app_test_db'
 }
--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-// Setup ===============================
-var express = require('express');
-var app = express();
+// set up ====================================
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var database = require('./config/database.js')
 var port = process.env.PORT || 8080;
-var morgan = require('morgan');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var database = require('./config/database.js');
 
-// Configuration ====================
+// Connect to DB =============================
+mongoose.connection.on('open', function (ref) {
+  console.log('Success: Connected to mongo server...');
+});
+mongoose.connection.on('error', function (err) {
+  console.log('Error: Could not connect to mongo server!');
+  console.log(err);
+});
 mongoose.connect(database.url);
 
-app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended':'true'}));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+//app config settings ========================
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-// Routes ==============================
+// Routes ====================================
 require('./app/routes.js')(app);
 
 // listen (start app with node server.js======
